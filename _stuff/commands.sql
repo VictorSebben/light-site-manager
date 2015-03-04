35d4fb7ea65364ae8264cf0ea75adec8f2455313
Fernando Basso <fernandobasso.br@gmail.com>
2015-03-04 20:51:14 -0300

    More sql sample commands.

--- _stuff/commands.sql
+++ _stuff/commands.sql
@@ -9,6 +9,11 @@ CREATE TABLE cat_users (
     description VARCHAR( 32 )
 );
 
+INSERT INTO cat_users ( description ) VALUES
+( 'admin' ),
+( 'editor' ),
+( 'blogger' );
+
 CREATE TABLE users (
     id SERIAL NOT NULL PRIMARY KEY,
     name VARCHAR( 64 ) NOT NULL,
@@ -17,3 +22,17 @@ CREATE TABLE users (
     cat_id INTEGER NOT NULL,
     FOREIGN KEY ( cat_id ) REFERENCES cat_users ( id )
 );
+
+
+INSERT INTO users (name, email, password, cat_id) VALUES
+( 'Yoda', 'yoda@jedi.net', '1234', 1 ),
+( 'Luke', 'luke.theforce.net', 4321, 2 ),
+( 'Vader', 'vader@darkside.net', 'asdf', 1 );
+
+CREATE ROLE ecommaster WITH
+    LOGIN PASSWORD '1234'
+    CREATEDB REPLICATION
+    VALID UNTIL 'infinity';
+
+
+
