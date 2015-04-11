77c2787c075634c298e66c23d77cf2c09ca904d4
Victor Sebben <victorhfsebben@gmail.com>
2015-04-11 18:45:14 -0300

    Remove unnecessary overriding method.

--- mappers/class.CatUsersMapper.php
+++ mappers/class.CatUsersMapper.php
@@ -8,16 +8,4 @@ class CatUsersMapper extends Mapper {
             "SELECT id, description FROM cat_users WHERE id = :id"
         );
     }
-
-    public function find( $id ) {
-        $this->_selectStmt->bindParam( ':id', $id );
-        $this->_selectStmt->execute();
-        $this->_selectStmt->setFetchMode( PDO::FETCH_CLASS, 'CatUsersModel' );
-        $catUser = $this->_selectStmt->fetch();
-        $this->_selectStmt->closeCursor();
-
-        if ( !$catUser ) return null;
-
-        return $catUser;
-    }
 }
