cab258f01d1692be78edbf70520930c039c5648c
Victor Sebben <victorhfsebben@gmail.com>
2015-11-21 18:10:19 -0200

    Reduce general width of the page

---
+++ views/users/delete.html.php
@@ -0,0 +1,16 @@
+<h2>Remover Role</h2>
+
+<div class="form-field confirm-delete">
+    <h3>Deseja realmente remover a Role "<?= $this->object->name ?>"?</h3>
+    <form action="<?= $this->Url->make( "roles/destroy" ) ?>" method="post">
+        <div class="form-field">
+            <input class="input-submit" type="submit" name="delete" value="Confirmar">
+        </div>
+
+        <!-- Token field -->
+        <input type="hidden" name="token" value="<?= H::generateToken() ?>">
+        <!-- Id field -->
+        <input type="hidden" name="id" value="<?= $this->object->id ?>">
+    </form>
+    <a class="input-submit" href="<?= $this->Url->make( 'roles/' ) ?>">Voltar</a>
+</div>
