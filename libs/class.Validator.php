c60b42fb89facbceb2fe0676527bf170dd36b38d
Victor Sebben <victorhfsebben@gmail.com>
2015-12-08 17:39:49 -0200

    Add new forms of validation
    Validator is now capable of validating regular
    expressions, as well as validating password
    fields, enforcing requirements.

--- libs/class.Validator.php
+++ libs/class.Validator.php
@@ -3,8 +3,19 @@
 class Validator {
 
     protected $_source;
+    protected $_items;
     public $errors;
     public $checkToken = true;
+
+    const MIN_PASSWD_REQUIREMENTS = 3;
+
+    /**
+     * Array of regular expressions for validation.
+     * @var array
+     */
+    public static $regexp = array(
+        'email' => ';(?:[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]);'
+    );
 
     //
     // Constants for white-list validation
@@ -21,12 +32,16 @@ class Validator {
      * The $items array contain the field names and rules to validate
      * the values present in the $source array.
      *
-     * @param array $source
-     * @param array $items
+     * @param array $source The source of the data (e.g. $_POST)
+     * @param array $items Items to be validated (e.g. name) and its rules
      * @return bool
      */
     public function check( array $source, array $items = array() ) {
+        // The source of the data (will generally be $_POST)
         $this->_source = $source;
+
+        // The items to be checked
+        $this->_items = $items;
 
         // test Token for CSFR protection
         if ( $this->checkToken ) {
@@ -36,11 +51,15 @@ class Validator {
             }
         }
 
-        foreach ( $items as $item => $rules ) {
-            if ( !isset( $source[ $item ] ) ) {
+        foreach ( $this->_items as $item => $rules ) {
+            // If the item to be checked is not present in the
+            // source array, just continue checking next items
+            if ( ! isset( $source[ $item ] ) ) {
                 continue;
             }
 
+            // If the value to be checked is actually an array
+            // (checkbox), call $this->checkArray()
             if ( is_array( $source[ $item ] ) ) {
                 $this->checkArray( $item, $rules );
             } else {
@@ -121,6 +140,8 @@ class Validator {
         {
             if ( $rule == 'required' && empty( $value ) ) {
                 $this->addError( ucfirst( $fieldName ) . ' é obrigatório.' );
+            } else if ( $rule == 'password' ) {
+                $this->checkPassword( $value );
             } else {
                 $rule = explode( ':', $rule );
 
@@ -154,7 +175,7 @@ class Validator {
                         $otherValue = filter_var( $this->_source[ $rule[ 1 ] ], FILTER_SANITIZE_SPECIAL_CHARS );
 
                         if ( $value != $otherValue ) {
-                            $this->addError( "Valor do campo " . ucfirst( $fieldName ) . " deve ser idêntico ao do campo {$rule[ 2 ]}." );
+                            $this->addError( "Valor do campo " . ucfirst( $fieldName ) . " deve ser idêntico ao do campo {$this->_items[ $rule[ 1 ] ][ 'fieldName' ]}." );
                         }
 
                         break;
@@ -177,16 +198,161 @@ class Validator {
                         }
 
                         break;
+
+                    case 'regex':
+                        if ( ! preg_match( self::$regexp[ $rule[ 1 ] ], $value ) ) {
+                            $this->addError( "Valor inválido para o campo {$fieldName}." );
+                        }
+
+                        break;
                 }
             }
         }
     }
 
+    /**
+     * @param $errorMsg
+     */
     protected function addError( $errorMsg ) {
         $this->errors[] = $errorMsg;
     }
 
+    /**
+     * @return string
+     */
     public function getErrorsJson() {
         return json_encode( $this->errors );
+    }
+
+    /**
+     * Checks if the password matches all password-complexity rules,
+     * adding errors for the rules that are not matched
+     *
+     * @param $password
+     */
+    protected function checkPassword( $password ) {
+        if ( ! $this->checkPassMin( $password ) ) {
+            $this->addError( 'A Senha deve ter no mínimo 10 caracteres.' );
+        } else if ( ! $this->checkPassMax( $password ) ) {
+            $this->addError( 'A Senha deve ter no máximo 128 caracteres.' );
+        }
+
+        $arrRules = $this->checkPassComplexity( $password );
+
+        $countRulesMet = count( array_filter( $arrRules, function ( $item ) {
+            return $item;
+        } ) );
+
+        // The password must meet at least 3 complexity requirements
+        if ( $countRulesMet < self::MIN_PASSWD_REQUIREMENTS ) {
+            $errorMsg = 'A senha deve conter ao menos '
+                . ( self::MIN_PASSWD_REQUIREMENTS - $countRulesMet ) . ' das características a seguir:';
+
+            $errorMsg .= '<ul>';
+
+            if ( ! $arrRules[ 'upper' ] ) {
+                $errorMsg .= '<li>pelo menos uma letra maiúscula.</li>';
+            }
+
+            if ( ! $arrRules[ 'lower' ] ) {
+                $errorMsg .= '<li>pelo menos uma letra minúscula.</li>';
+            }
+
+            if ( ! $arrRules[ 'digit' ] ) {
+                $errorMsg .= '<li>pelo menos um dígito.</li>';
+            }
+
+            if ( ! $arrRules[ 'schar' ] ) {
+                $errorMsg .= '<li>pelo menos um dos seguintes caracteres: '
+                    . '\', ", @, #, $, %, &, *, (, ), -, _, +, =, ´, `, ^, ~, <, >, '
+                    . '/, \\, ?, !, :, ;, |, [, ], {, }, ç, Ç, ¨</li>';
+            }
+
+            $errorMsg .= '</ul>';
+
+            $this->addError( $errorMsg );
+        }
+
+        // Check if password contains three identical
+        // chars in a a row
+        if ( $arrRules[ 'trow' ] ) {
+            $this->addError( 'A Senha não pode conter 3 caracteres iguais consecutivos.' );
+        }
+    }
+
+    /**
+     * @param $password
+     * @return bool
+     */
+    protected function checkPassMin( $password ) {
+        if ( strlen( $password ) < 10 ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param $password
+     * @return bool
+     */
+    protected function checkPassMax( $password ) {
+        if ( strlen( $password ) > 128 ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks for password complexity.
+     * Returns an array showing if the 4
+     * rules of complexity are met, and
+     * if 3 identical characters in a row
+     * were found.
+     *
+     * @return array
+     */
+    protected function checkPassComplexity( $password ) {
+        // We'll count the amount of complexity rules met.
+        // To be safe, the password must meet at least 3.
+        $arrRules = [
+            'upper' => false, // upper-case letter
+            'lower' => false, // lower-case letter
+            'digit' => false, // numerical value
+            'schar' => false  // special characters
+        ];
+
+        // Check if there are three identical characters in a row:
+        $arrRules[ 'trow' ] = false;
+
+        for ( $i = 0, $count = 1; $i < strlen( $password ); $i++ ) {
+            $char = $password[ $i ];
+
+            if ( ctype_upper( $char ) ) {
+                $arrRules[ 'upper' ] = true;
+            } else if ( ctype_lower( $char ) ) {
+                $arrRules[ 'lower' ] = true;
+            } else if ( ctype_digit( $char ) ) {
+                $arrRules[ 'digit' ] = true;
+            } else if ( H::isSpecialChar( $char ) ) {
+                $arrRules[ 'schar' ] = true;
+            }
+
+            // Test if it's third character in a row
+            if ( $i != 0 ) {
+                if ( $char == $password[ $i - 1 ] ) {
+                    $count++;
+                    if ( $count == 3 ) {
+                        // Three identical chars in a row:
+                        // set trow to true
+                        $arrRules[ 'trow' ] = true;
+                        $count = 0;
+                    }
+                }
+            }
+        }
+
+        return $arrRules;
     }
 }
